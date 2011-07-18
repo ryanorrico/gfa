be7c6d3fb42d20e4929ec9d925e1cfa02a3d5670
Ryan Orrico <ryanorrico@gmail.com>
2011-07-17 21:29:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfa::Application.config.secret_token = '46e35cf22de8b18ddb99cd52fd0bd5ca491faffb9776f11c5e8d81a6e753fe2396adc66e289eee3bdef2f5d1d47a1476a6d05cbba85873dff9198643aaeff320'
