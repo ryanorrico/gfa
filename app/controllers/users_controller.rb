f7227f4f8bcab926aa0be13e9f87bf5404f56d8c
Ryan Orrico <ryanorrico@gmail.com>
2011-07-19 12:16:45 -0700

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,24 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to Get Fucking Awesome!"
+    else
+      @title = "Sign up"
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,9 +38,6 @@ describe UsersController do
   end # GET 'show'
   
   
-  
-  
-  
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -51,6 +48,83 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end  #should have the right title
+    
+    describe "testing for fields" do
+      
+      it "should have a name field" do
+        get :new
+        response.should have_selector("input[name='user[name]'][type='text']")
+      end  #should have a name field
+      
+      it "should have an email field" do
+        get :new
+        response.should have_selector("input[name='user[email]'][type='text']")
+      end  #should have a email field
+      
+      it "should have a password field" do
+        get :new
+        response.should have_selector("input[name='user[password]'][type='password']")
+      end  #should have a password field
+      
+      it "should have a password confirmation field" do
+        get :new
+        response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+      end  #should have a password confirmation field
+      
+    end # testing for fields
   end
-
+  
+  
+  
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => ""}
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end  #should not create a user
+  
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector('title', :content => "Sign up")
+      end  #should have the right title
+      
+      it "should render the the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end  #should render the the 'new' page
+            
+    end # failure
+    
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com", 
+                  :password => "foobar", :password_confirmation => "foobar" }
+      end
+                  
+        it "should create a user" do
+          lambda do
+            post :create, :user => @attr
+          end.should change(User, :count).by(1)
+        end  #should create a user
+        
+        it "should redirect to the user show page" do
+          post :create, :user => @attr
+          response.should redirect_to(user_path(assigns(:user)))
+        end  #should redirect to the user show page
+        
+        it "should display a welcome message" do
+          post :create, :user => @attr
+          flash[:success].should =~ /welcome to Get Fucking Awesome/i
+        end  #should display a welcome message
+        
+    end # success
+  end # POST 'create'
 end
